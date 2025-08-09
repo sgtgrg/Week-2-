551c2d117ab70235ae4132c3ca4b4561feea7671
sgtgrg <270738626@yoobeestudent.ac.nz>
2025-08-09 15:44:29 +1200

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -1,36 +1,48 @@
 import random
 
-words = ["characterstics", "wizard", "football", "wuthering", "pneumonoultramicroscopicsilicovolcanoconiosis"]
-
-word = random.choice(words)
-
-blank = ["_"] * len(word)
-
-lives = 5
-
-print("Welcome to Word Guessinfg Game!")
-print("_".join(blank))
-
-while lives > 0:
-    guess = input("Guess a letter :")
+class GuessTheWordGame:
+    def __init__(self):
+        self.words = ["characterstics", "wizard", "football", "wuthering", "pneumonoultramicroscopicsilicovolcanoconiosis"]
+        self.word = random.choice(self.words)
+        self.blank = ["_"] * len(self.word)
+        self.lives = 5
+        self.already_guessed = set()
     
-    if not guess.isalpha() or len(guess) != 1:
-      print("Invalid entry! Enter a valid input!")
-      continue
+    def start_game(self):
+     ("Welcome to Word Guessinfg Game!")
+     print("_".join(self.blank))
+
+     while self.lives > 0:
+      guess = input("Guess a letter :").lower()
     
-    if guess in word:
-        for i in range(len(word)):
-           if word [i] == guess:
-               blank[i] = guess
-    else: 
-        lives -= 1 
-        print("Wrong guess! Lives left:",(lives))
+      if not guess.isalpha() or len(guess) != 1:
+       print("Invalid entry! Enter a valid input!")
+       continue
+    
+      if guess in self.already_guessed:
+        print("You have already guessed that letter. Choose a different letter")
+        continue
+    
+      self.already_guessed.add(guess)
+    
+      if guess in self.word:
+        print("Good! You guessed one!")
+        for i in range(len(self.word)):
+           if self.word [i] == guess:
+               self.blank[i] = guess
+        print("_".join(self.blank))
+      else: 
+        self.lives -= 1 
+        print("Wrong guess! Lives left:",(self.lives))
+        print("_".join(self.blank))
         
-    print("_".join(blank))
-    
-    if "_" not in blank:
-        print("You win! The word was:", word)
+      if "_" not in self.blank:
+        print("You win! The word was:", self.word)
         break
+     if "_" in self.blank:
+      print("Game Over! The word is:", self.word)
 
-if "_" in blank:
-    print("Game Over! The word is:", word)
+game = GuessTheWordGame()
+game.start_game()
+    
+    
